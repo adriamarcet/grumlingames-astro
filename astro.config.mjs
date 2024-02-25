772c95eb62b75f906a2f89f0ee6364ffc56bd726
Adrià Marcet <amarcet@runroom.com>
2024-02-26 00:43:29 +0100

    Add menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://www.grumlingames.com',  
-    i18n: {
-      defaultLocale: "ca",
-      locales: ["ca", "es", "en"],
-      routing: {
-        prefixDefaultLocale: false
-      }
+  site: 'https://www.grumlingames.com',
+  i18n: {
+    defaultLocale: "ca",
+    locales: ["ca", "es", "en"],
+    routing: {
+      prefixDefaultLocale: false
     }
-  })
+  },
+  integrations: [tailwind()]
+});
