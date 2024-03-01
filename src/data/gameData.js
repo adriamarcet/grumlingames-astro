03d9cc633b8c3311118df10857db45fae499f6b4
Adrià Marcet <amarcet@runroom.com>
2024-03-01 17:50:41 +0100

    Revamp nav and colors

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -1,15 +1,21 @@
 const gameData = [
     {
         name: 'Magimals',
-        path: '/magimals'
+        path: '/magimals',
+        description: "",
+        image: "https://dummyimage.com/736x422/000/fff.jpg&text=MAGIMALS"
     },
     {
         name: 'Misty Valley',
-        path: '/misty-valley'
+        path: '/misty-valley',
+        description: "",
+        image: "https://dummyimage.com/736x422/000/fff.jpg&text=MISTY+VALLEY"
     },
     {
         name: 'Galió Goblin',
-        path: '/galio-goblin'
+        path: '/galio-goblin',
+        description: "",
+        image: "https://dummyimage.com/736x422/000/fff.jpg&text=GALIO+GOBLIN"
     }
 ];
 
