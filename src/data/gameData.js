b040b79c2c20c53731962c14ce8a48a2177428c4
Adrià Marcet <amarcet@runroom.com>
2024-03-01 17:52:53 +0100

    Add dimensions to image boxes

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -3,19 +3,19 @@ const gameData = [
         name: 'Magimals',
         path: '/magimals',
         description: "",
-        image: "https://dummyimage.com/736x422/000/fff.jpg&text=MAGIMALS"
+        image: "https://dummyimage.com/736x422/000/fff.jpg&text=MAGIMALS+Width-736+height-422"
     },
     {
         name: 'Misty Valley',
         path: '/misty-valley',
         description: "",
-        image: "https://dummyimage.com/736x422/000/fff.jpg&text=MISTY+VALLEY"
+        image: "https://dummyimage.com/736x422/000/fff.jpg&text=MISTY+VALLEY+Width-736+height-422"
     },
     {
         name: 'Galió Goblin',
         path: '/galio-goblin',
         description: "",
-        image: "https://dummyimage.com/736x422/000/fff.jpg&text=GALIO+GOBLIN"
+        image: "https://dummyimage.com/736x422/000/fff.jpg&text=GALIO+GOBLIN+Width-736+height-422"
     }
 ];
 
