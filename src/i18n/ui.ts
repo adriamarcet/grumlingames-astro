0fd791b103bc67d139c7992738ccbfa8227d3296
Adrià Marcet <amarcet@runroom.com>
2024-06-04 17:49:08 +0200

    WIP

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -21,6 +21,9 @@ export const strings = {
         'navbarmenu.altlogo': 'Logotip de Grumlin Games',
         'head.home_page': "Pàgina d'inici",
         'footer.all_rights_reserved': 'Tots els drets reservats',
+        'page.home.h1': "Jocs grans en caixes petites",
+        'page.home.intro': "Benvingut a Grumlin Games. A Grumlin Games, creem emocionants i memorables jocs de taula per a totes les edats. El nostre primer llançament, 'Misty Valley', és només el principi.",
+        'games.section_title': "Mira les nostres propostes"
     },
     es: {
         'common.games': 'Juegos',
@@ -32,6 +35,9 @@ export const strings = {
         'navbarmenu.altlogo': 'Logotipo de Grumlin Games',
         'head.home_page': "Página de inicio",
         'footer.all_rights_reserved': 'Todos los derechos reservados',
+        'page.home.h1': "Pequeños juegos en grandes cajas",
+        'page.home.intro': "Bienvenido a Grumlin Games. En Grumlin Games, creamos emocionantes y memorables juegos de mesa para todas las edades. Nuestro primer lanzamiento, 'Misty Valley', es solo el principio.",
+        'games.section_title': "Mira nuestras propuestas"
     },
     en: {
         'common.games': 'Games',
@@ -43,5 +49,8 @@ export const strings = {
         'navbarmenu.altlogo': 'Grumlin Games logo',
         'head.home_page': "Home page",
         'footer.all_rights_reserved': 'All rights reserved',
+        'page.home.h1': "Big games on small boxes",
+        'page.home.intro': "Welcome to Grumlin Games. At Grumlin Games, we create exciting and memorable board games for all ages. Our first release, 'Misty Valley', is just the beginning.",
+        'games.section_title': "Check out our proposals"
     },
 } as const;
